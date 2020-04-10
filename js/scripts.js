f2fc011042eef0072ac369da93faa1aa788db41d
NeverSettels <settels.eric@gmail.com>
2020-04-10 08:41:13 -0700

    add constructors for order and pizza

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Order() {
+    this.pizzas = [];
+    this.priceTotal = 0;
+}
+
+//calculate total methos tbc
+
+function Pizza() {
+    this.toppings = [];
+    this.crust = '';
+    this.sauce = '';
+    this.price = 10;
+}
